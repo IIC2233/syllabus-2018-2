8c6d77cadf2f9804411b6d53d88e6847c7ceb868
Rodrigo Hanuch <rihanuch@uc.cl>
2018-09-03 22:27:19 -0300

    Added main.py for single while use 🚀

--- Tareas/T01/main.py
+++ Tareas/T01/main.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
             # rellenar esta parte con el llamado a sus funciones
             # sigue corriendo el uso restringido en toda situacion
             # de los for/while/etc dentro de este main a excepcion
-            # del que se encuentra arriba
+            # del que se encuentra arriba, por lo que no se puede
+            # agregar ninguno mas
 
     # no es necesario que hagan una parte para salir del menu
     except KeyboardInterrupt():
