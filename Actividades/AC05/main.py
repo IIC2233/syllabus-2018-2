e120b22ab78022a9f736dfebeeb900a2ce2b573d
entamburini <entamburini@uc.cl>
2018-09-13 14:29:59 -0300

    Enunciado AC05

---
+++ Actividades/AC05/main.py
@@ -0,0 +1,9 @@
+# Aqui abajo debes escribir el código de tus clases
+
+
+if __name__ == '__main__':
+    """
+    A continuación debes instanciar cada uno de los objetos pedidos,
+    para que puedas simular la batalla.
+    """
+    pass
