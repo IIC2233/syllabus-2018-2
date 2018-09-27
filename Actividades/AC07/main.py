8775edf91f8361d163299c8901140fbe7da20aa3
fringlesinthestreet <bicarcamo@uc.cl>
2018-09-27 14:24:44 -0300

    main file added

---
+++ Actividades/AC07/main.py
@@ -0,0 +1,52 @@
+
+
+
+class Terreno:
+
+    def __init__(self, nombre):
+        pass
+
+
+
+class Ciudad:
+
+    def __init__(self, path):
+        pass
+
+    def agregar_calle(self, origen, destino):
+        pass
+
+    def eliminar_calle(self, origen, destino):
+        pass
+
+    @property
+    def terrenos(self):
+        pass
+
+    @property
+    def calles(self):
+        pass
+
+    def verificar_ruta(self, ruta):
+        pass
+
+    def entregar_ruta(self, origen, destino):
+        pass
+
+
+    def ruta_corta(self, origen, destino):
+        pass
+
+
+    def ruta_entre_bombas(self, origen, *destinos):
+        pass
+
+    def ruta_corta_entre_bombas(self, origen, *destinos):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    pass
+    # Agrega aqui tus consultas
